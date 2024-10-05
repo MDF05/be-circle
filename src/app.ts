9d23f6f7b83b905e1d8ae3377287b2fbd6e23b9c
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-10-05 11:32:56 +0700

    deploy 11

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use("/assets", express.static("./src/image"))
+app.use("/assets", express.static("./src/image"))
 
 
 app.get("/", (req, res) => {
