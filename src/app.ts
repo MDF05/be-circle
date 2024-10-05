5adff79b36decb66f8fc67be824551619d031b65
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-10-05 11:09:55 +0700

    fix: text deploy 1

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
-// import createError from "./v1/utils/create-error";
+import createError from "./v1/utils/create-error";
 // import { PrismaClient } from "@prisma/client";
 
 
-// import RouterV1 from "./version/v1";
-// import RouterV2 from "./version/v2";
-// import errorResponse from "./v1/utils/error-response";
+import RouterV1 from "./version/v1";
+import RouterV2 from "./version/v2";
+import errorResponse from "./v1/utils/error-response";
 // import cors from 'cors';
 
 
@@ -25,16 +25,13 @@ const port = process.env.PORT || 3000
 
 
 
-// app.use("/api/v1", RouterV1)
-// app.use("/api/v2", RouterV2)
+app.use("/api/v1", RouterV1)
+app.use("/api/v2", RouterV2)
 
 
-// app.use("/", (req: Request, res: Response, next: NextFunction) => next(createError("PAGE NOT FOUND", 404)))
-// app.use(errorResponse)
+app.use("/", (req: Request, res: Response, next: NextFunction) => next(createError("PAGE NOT FOUND", 404)))
+app.use(errorResponse)
 
-app.get("/asu", (req: Request, res: Response, next: NextFunction) => {
-    res.json({ name: "jame" })
-})
 
 app.listen(port, async () => {
     // await Prisma.$connect()
