4aaf7780a3e8e73d5990cf6bfa80fe042e97b79e
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-10-05 13:49:17 +0700

    fix: prisma generate

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express, { NextFunction, Request, Response, Express } from "express";
 import createError from "./v1/utils/create-error";
-import { PrismaClient } from "@prisma/client";
 
 
-// import RouterV1 from "./version/v1";
-// import RouterV2 from "./version/v2";
+import RouterV1 from "./version/v1";
+import RouterV2 from "./version/v2";
 import errorResponse from "./v1/utils/error-response";
 import cors from 'cors';
 
@@ -27,8 +26,8 @@ app.use("/assets", express.static("./src/image"))
 app.get("/", (req, res) => {
     res.json({ message: "Hello, Worlda!" })
 })
-// app.use("/api/v1", RouterV1)
-// app.use("/api/v2", RouterV2)
+app.use("/api/v1", RouterV1)
+app.use("/api/v2", RouterV2)
 
 
 app.use("/", (req: Request, res: Response, next: NextFunction) => next(createError("PAGE NOT FOUND", 404)))
@@ -36,7 +35,6 @@ app.use(errorResponse)
 
 
 app.listen(port, async () => {
-    await Prisma.$connect()
     console.log("berhasil connect ke database")
     console.log(`listening on port ${port}`)
 })
