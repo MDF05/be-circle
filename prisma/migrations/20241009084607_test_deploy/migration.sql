0426a08e105bd431dea1483eda6c91dfd3260b1f
dava <mdavafahrez05@gmail.com>
2024-10-09 16:02:15 +0700

    fix: deploy default image

---
+++ prisma/migrations/20241009084607_test_deploy/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "profile" ALTER COLUMN "cover" SET DEFAULT 'https://be-circle-one.vercel.app/api/v1cover.png',
+ALTER COLUMN "image" SET DEFAULT 'https://be-circle-one.vercel.app/api/v1avatar.png';
