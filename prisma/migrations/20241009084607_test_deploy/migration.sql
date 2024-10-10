8338e33e831f154b45232dc764b2e6a5ca1a94ec
dava <mdavafahrez05@gmail.com>
2024-10-10 11:23:06 +0700

    feature: deployment succes

--- prisma/migrations/20241009084607_test_deploy/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "profile" ALTER COLUMN "cover" SET DEFAULT 'https://be-circle-one.vercel.app/api/v1cover.png',
-ALTER COLUMN "image" SET DEFAULT 'https://be-circle-one.vercel.app/api/v1avatar.png';
