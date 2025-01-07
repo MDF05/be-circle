329232d0f8cc5094c33c55cd61aa67377877580e
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-01-07 20:30:13 +0700

    test: cloudinary

--- src/v1/controllers/thread-controller.ts
+++ src/v1/controllers/thread-controller.ts
@@ -9,7 +9,6 @@ class ThreadController {
   async post(req: RequestExtUser, res: Response, next: NextFunction) {
     try {
       const body = { ...req.body, profileId: req?.user?.profile.id };
-      console.log(req.file);
 
       if (req?.file) body.image = await cloudinary.uploader(req.file);
 
