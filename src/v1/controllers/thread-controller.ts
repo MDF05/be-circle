c80431114488099a140c5bac09bdb6e5f66ed882
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-01-07 16:19:10 +0700

    try: body image:

--- src/v1/controllers/thread-controller.ts
+++ src/v1/controllers/thread-controller.ts
@@ -11,6 +11,7 @@ class ThreadController {
       console.log(req.file);
       const body = { ...req.body, profileId: req?.user?.profile.id };
       if (req?.file) body.image = await cloudinary.uploader(req.file);
+      console.log(body);
 
       const post = await postService.create(body);
       succesResponse(res, "post created successfully", 201, post);
